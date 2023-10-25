cbd6fbb2f21b1b87567b1c5d70ea516f1d4587ca
chromaLTS <kasper50@live.dk>
2023-10-25 15:25:26 +0200

    trying to fix git problem

---
+++ react-project/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+
+function MenuItem(props) {
+  function handleClick() {
+    console.log("test")
+  }
+  return (
+    <div className="MenuItem" onClick={() => handleClick}>
+      <h2>{props.name}</h2>
+      <p>{props.description}</p>
+    </div>
+  )
+}
+
+
+function Menu() {
+  return (
+    <div className="Menu">
+      <MenuItem name="Minecraft Server" description="Its a minecraft server" />
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PC Control Panel</h1>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
