c088a77c67aeeec71eb6c624cf25907afaf2fed6
chromaLTS <kasper50@live.dk>
2023-11-17 15:24:10 +0100

    getting websockets to work

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,16 +1,15 @@
 import './App.css';
-import React, { useState, useCallback, useEffect }  from 'react';
-import useWebSocket, { ReadyState }  from 'react-use-websocket';
+import React, {useRef, useEffect} from 'react';
 
 
 function MenuItem(props) {
   function handleClick() {
     console.log("test")
   }
+
   return (
-    <div className="MenuItem" onClick={() => handleClick}>
-      <h2>{props.name}</h2>
-      <p>{props.description}</p>
+    <div className="MenuItem" onClick={() => handleClick()}>
+      <p className="Title">{props.name}</p>
     </div>
   )
 }
@@ -19,60 +18,37 @@ function MenuItem(props) {
 function Menu() {
   return (
     <div className="Menu">
-      <MenuItem name="Minecraft Server" description="Its a minecraft server" />
+      <MenuItem name="Minecraft Server" description="Its a minecraft server"/>
+      <MenuItem name="Home PC" description="Its a minecraft server"/>
     </div>
   )
 }
 
 function App() {
-  const [socketUrl, setSocketUrl] = useState('wss://localhost:443');
-  const [messageHistory, setMessageHistory] = useState([]);
-
-  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl, {});
+  const ws = useRef(null);
 
   useEffect(() => {
-    if (lastMessage !== null) {
-      setMessageHistory((prev) => prev.concat(lastMessage));
-    }
-  }, [lastMessage, setMessageHistory]);
+      console.log("connecting")
+      ws.current = new WebSocket(`ws://${window.location.hostname}:8443/`);
+      ws.current.onopen = () => console.log("ws opened");
+      ws.current.onclose = () => console.log("ws closed");
 
-  const handleClickChangeSocketUrl = useCallback(
-    () => setSocketUrl('wss://localhost:443'),
-    []
-  );
+      const wsCurrent = ws.current;
 
-  const handleClickSendMessage = useCallback(() => sendMessage('Hello'), []);
-
-  const connectionStatus = {
-    [ReadyState.CONNECTING]: 'Connecting',
-    [ReadyState.OPEN]: 'Open',
-    [ReadyState.CLOSING]: 'Closing',
-    [ReadyState.CLOSED]: 'Closed',
-    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
-  }[readyState];
+      return () => {
+          wsCurrent.close();
+      };
+  }, []);
 
   return (
     <div className="App">
-      <h1>PC Control Panel</h1>
       <Menu />
-      <br />
-      <div>
-        <button onClick={handleClickChangeSocketUrl}>
-          Click Me to change Socket Url
-        </button>
-        <button
-          onClick={handleClickSendMessage}
-          disabled={readyState !== ReadyState.OPEN}
-        >
-          Click Me to send 'Hello'
-        </button>
-        <span>The WebSocket is currently {connectionStatus}</span>
-        {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
-        <ul>
-          {messageHistory.map((message, idx) => (
-            <span key={idx}>{message ? message.data : null}</span>
-          ))}
-        </ul>
+      <div className='gridContainer'>
+        <div className='content'>
+          <h1>PC Control Panel</h1>
+          <br />
+          <button onClick={() => ws.current.send("test")}>test</button>
+        </div>
       </div>
     </div>
   );
